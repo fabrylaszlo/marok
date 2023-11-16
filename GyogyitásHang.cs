1caeb59a3c4280c7eca7d4d0c444ea1e3bd197c1
konya7 <73104976+konya7@users.noreply.github.com>
2023-11-16 12:19:42 +0100

    Gyógyítás hang

---
+++ GyogyitásHang.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyogyitásHang : MonoBehaviour
+{
+    public AudioClip Zenelejatszo;
+    public float Hangero;
+    AudioSource zene;
+    public bool FolyamatosLejatszas = false;
+
+    public bool healhangstop = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        zene = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mozgas.HealHang == true)
+        {
+            if (!FolyamatosLejatszas)
+            {
+                zene.PlayOneShot(Zenelejatszo, Hangero);
+                FolyamatosLejatszas = true;
+                Mozgas.HealHang = false;
+            }
+        }
+        if (Mozgas.HealHang == false)
+        {
+            if (FolyamatosLejatszas == true)
+            {
+                zene.Stop();
+                FolyamatosLejatszas = false;
+            }
+        } 
+    }
+}
