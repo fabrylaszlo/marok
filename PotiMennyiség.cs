b291dde524ef854c6a359b14f60e7457ee21565b
konya7 <73104976+konya7@users.noreply.github.com>
2023-11-16 13:53:52 +0100

    Poti számláló

---
+++ PotiMennyiség.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PotiMennyiség : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        text.text = $"{Mozgas.HealPoti}";
+    }
+}
