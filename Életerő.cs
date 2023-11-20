6c9be5fb94f1b9cf766d4eb3bef5334254b93543
konya7 <73104976+konya7@users.noreply.github.com>
2023-11-20 18:04:39 +0100

    Játékos életereje

---
+++ Életerő.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Életerő : MonoBehaviour
+{
+    public Slider Életcsúszka;
+    public Gradient Szinskála;
+    public Image Kitöltés;
+    public void SetÉlet(int élet)
+    {
+        Életcsúszka.value = élet;
+        Kitöltés.color = Szinskála.Evaluate(Életcsúszka.normalizedValue);
+    }
+
+    public void SetMaxÉlet(int élet)
+    {
+        Életcsúszka.maxValue = élet;
+        Életcsúszka.value = élet;
+        Kitöltés.color = Szinskála.Evaluate(1f);
+    }
+}
