1f7ada70873934a27142a3c88fd0a29efd6fbb49
Fábry László <70687838+fabrylaszlo@users.noreply.github.com>
2023-11-20 19:57:39 +0100

    Add files via upload
    kezdőmenu gombok

---
+++ mainmenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{
+    public void Playgame(){
+        SceneManager.LoadScene(0);
+    }
+  public void Quitgame(){
+       Application.Quit();
+    }
+ 
+}
